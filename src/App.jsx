0fa3151e10d095abaa24a1382e6cfd1a5c80fb91
Jayanta <jbtistasoft@gmail.com>
2024-05-14 23:32:50 +0530

    react portfolio first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './view/Home';
+import  '../public/css/home.css';
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
